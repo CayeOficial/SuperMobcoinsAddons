9057d7e2f47ec64f5fcff47e1e77e60b79e317a6
CayeOficial <73038322+CayeOficial@users.noreply.github.com>
2022-05-25 15:39:30 +0200

    Fixed Memory Leak

--- MobcoinsHelpers/src/main/java/com/cayeoficial/listeners/MobDeath.java
+++ MobcoinsHelpers/src/main/java/com/cayeoficial/listeners/MobDeath.java
@@ -23,47 +23,38 @@ public class MobDeath implements Listener {
     public MobDeath(Main plugin) {
         this.plugin = plugin;
     }
+    Random random = new Random();
     @EventHandler
     public void onMobDeath(EntityDeathEvent event) {
-    Player killer = event.getEntity().getKiller();
-    if(killer != null) {
-        ItemStack itemInOffHand = killer.getInventory().getItemInOffHand();
-        if(itemInOffHand.getType() == Material.ENCHANTED_BOOK) {
-            ItemMeta itemMeta = itemInOffHand.getItemMeta();
-            //Multiplicador x1.5
-            //Lore x1.5
-            List<String> listx15 = new ArrayList<String>();
-            listx15.add("§7§o(( Este multiplicador hace que tengas 5% más");
-            listx15.add("§7§ode posibilidad de conseguir un Mobcoin ))");
-            listx15.add("§r ");
-            listx15.add("§dPara usarlo debes equipartelo en tu");
-            listx15.add("§dmano secundaria y matar mobs.");
-            //Check x1.5
-            if(checkItemMeta(itemMeta, "§5§lMULTIPLICADOR§f | §dMobCoins 5%", listx15) && checkEntity(event.getEntity())) {
-                Random random = new Random();
-                int posibilidad = random.nextInt(100);
-                if(posibilidad < 7) {
-                    Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "mobcoins give " + killer.getName() + " 1");
-                }
-            }
+        Player killer = event.getEntity().getKiller();
+        if(killer != null) {
+            ItemStack itemInOffHand = killer.getInventory().getItemInOffHand();
+            if(itemInOffHand.getType() == Material.ENCHANTED_BOOK) {
+                ItemMeta itemMeta = itemInOffHand.getItemMeta();
 
-            //Multiplicador x2
-            //Lore x2
-            List<String> listx2 = new ArrayList<String>();
-            listx2.add("§7§o(( Este multiplicador hace que tengas 10% más");
-            listx2.add("§7§ode posibilidad de conseguir un Mobcoin ))");
-            listx2.add("§r ");
-            listx2.add("§dPara usarlo debes equipartelo en tu");
-            listx2.add("§dmano secundaria y matar mobs.");
-            //Check x1.5
-            if(checkItemMeta(itemMeta, "§5§lMULTIPLICADOR§f | §dMobCoins 10%", listx2) && checkEntity(event.getEntity())) {
-                Random random = new Random();
-                int posibilidad = random.nextInt(100);
-                if(posibilidad < 10) {
-                    Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "mobcoins give " + killer.getName() + " 1");
+                //Multiplicador x1.5
+                List<String> listx15 = new ArrayList<String>();
+                listx15.add("§7§o(( Este multiplicador hace que tengas 5% más");
+                listx15.add("§7§ode posibilidad de conseguir un Mobcoin ))");
+                if(checkItemMeta(itemMeta, "§5§lMULTIPLICADOR§f | §dMobCoins 5%", listx15) && checkEntity(event.getEntity())) {
+                    int posibilidad = random.nextInt(100);
+                    if(posibilidad <= 5) {
+                        Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "mobcoins give " + killer.getName() + " 1");
+                    }
                 }
+
+                //Multiplicador x2
+                List<String> listx2 = new ArrayList<String>();
+                listx2.add("§7§o(( Este multiplicador hace que tengas 7.5% más");
+                listx2.add("§7§ode posibilidad de conseguir un Mobcoin ))");
+                if(checkItemMeta(itemMeta, "§5§lMULTIPLICADOR§f | §dMobCoins 7.5%", listx2) && checkEntity(event.getEntity())) {
+                    int posibilidad = random.nextInt(100);
+                    if(posibilidad <= 7.5) {
+                        Bukkit.getServer().dispatchCommand(getServer().getConsoleSender(), "mobcoins give " + killer.getName() + " 1");
+                    }
+                }
+
             }
-      }
+        }
     }
-  }
 }
